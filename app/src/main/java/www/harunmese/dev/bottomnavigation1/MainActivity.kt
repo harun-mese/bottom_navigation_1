cff9aee2d1b8df2a0a3227bdbe382f18dc018433
harun-mese <harunmese13@gmail.com>
2024-02-26 07:55:24 +0300

    Initial commit

---
+++ app/src/main/java/www/harunmese/dev/bottomnavigation1/MainActivity.kt
@@ -0,0 +1,26 @@
+package www.harunmese.dev.bottomnavigation1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import www.harunmese.dev.bottomnavigation1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigationView,navController)
+    }
+}
